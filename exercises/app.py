a8ee671fd56c99135be967a6eccd057d1776f294
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 12:16:12 +0300

    route

--- exercises/app.py
+++ exercises/app.py
@@ -4,6 +4,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'START HERE: CHANGE ME'
+    return 'START HERE: yeahh!!'
+
+# @app.route("/")
+# def hello_name_route(name):
+#     return render_template(
+#         'hello.html', n = name)
+
+
+@app.route('/hello/<string:name>')
+def hello_name_route(name):
+    return render_template(
+        'hello.html', n = name)
+
+# @app.route("/")
+# def home_route(name):
+#     return render_template('home_with_link.html')
+
+@app.route("/hello/")
+def hello_route(name):
+    return render_template('hello_with_link.html')
+
+
+
 
 app.run(debug=True)
